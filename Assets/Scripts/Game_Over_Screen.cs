e93154bfbf035c39996bff8f1739e40e576e87cb
VasiliyMost <97123950+VasiliyMost@users.noreply.github.com>
2023-02-05 14:37:09 +0100

    game over screen

---
+++ Assets/Scripts/Game_Over_Screen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Over_Screen : MonoBehaviour
+{
+    [SerializeField] private GameObject ShitButton;
+    [SerializeField] private AudioSource ClickSFX;
+    private bool Pressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Button", 3f);
+    }
+
+    void Button()
+    {
+        ShitButton.SetActive(true);
+    }
+
+    public void PRESSME()
+    {
+        ClickSFX.Play();
+        if (!Pressed)
+        {
+            Pressed = true;
+            Invoke("loadscene", 0.5f);
+        }
+    }
+
+    void loadscene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
